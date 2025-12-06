b1abacc458297f22b9a3dfadf02de9fe13650799
Dilina <dilinacperera@gmail.com>
2025-12-06 21:43:03 +0530

    feat(login): implemented the validation & improved some style

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,54 +1,114 @@
 "use client"
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { FcGoogle } from 'react-icons/fc';
 
-export default function LoginPage() {
+ const LoginForm = () =>{
         const router = useRouter();
+        const [error, setError] = useState<{ email?: string; password?: string }>({});
+        const [formData, setFormData] = useState({
+            email: '',
+            password: '',
+        });
 
-        const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+        const validationForm = (data:any) => {
+            let error: { email?: string; password?: string } = {};
+            if(!data.email.trim()){
+                error.email = 'Email is required';
+            } else if (!/\S+@\S+\.\S+/.test(data.email)){
+                error.email = 'Email is Invalid';
+            }
+            if(!data.password){
+                error.password = 'Password is required';
+            } else if (data.password.length < 4){
+                error.password = 'Password must be at least 4 characters'
+            }
+            return error;
+        };
+
+        const handleChange = (e: any) => {
+            const {name, value} = e.target;
+            setFormData({
+                ...formData,
+                [name]: value,
+            });
+        };
+
+        const handleSubmit = (e: any) => {
             e.preventDefault();
-            router.push('/pages/dashboard');
-        }
+            const validationErrors = validationForm(formData);
+            setError(validationErrors);
+
+            if (Object.keys(validationErrors).length === 0){
+                console.log('Form Data: ', formData)
+                router.push('/pages/dashboard');
+            }
+        };
 
     return (
         <>
-            <div className='flex items-center justify-center min-h-screen'>
-                <div className="box-border border p-6 rounded-lg bg-blue-200">
+            <div className='flex items-center justify-center min-h-screen bg-blue-300'>
+                <div className="box-border p-6 rounded-lg bg-white">
                     <div className="text-center text-lg font-bold">
-                        <h1>Login Page</h1>
+                        <h1>Login</h1>
                     </div>
-                    <form className="flex flex-col gap-4 mt-4 w-96 items-center">
+                    <form onSubmit={handleSubmit}
+                        className="flex flex-col gap-4 mt-4 w-96 items-center">
                         <div>
                             <input
                                 className="w-96 px-4 py-2 rounded-lg focus:outline-none focus:ring-1 border bg-white" 
                                 type="email" 
-                                name="Email" 
-                                placeholder="Enter Your Email"
+                                id='email'
+                                name="email" 
+                                placeholder="Email"
+                                value={formData.email}
+                                onChange={handleChange}
                             />
+                            {error.email && <span className='text-red-500 text-sm mt-1'>{error.email}</span>}
                         </div>
                         <div>
                             <input 
                                 className="w-96 px-4 py-2 rounded-lg focus:outline-none focus:ring-1 border bg-white"
                                 type="password" 
-                                name="Password" 
-                                placeholder="Enter Your Password"
+                                id='password'
+                                name="password" 
+                                placeholder="Password"
+                                value={formData.password}
+                                onChange={handleChange}
                             />
+                            {error.password && <span className='text-red-500 text-sm mt-1'>{error.password}</span>}
                         </div>
+                        <button className='text-blue-600 ml-2 cursor-pointer text-sm'>
+                            Forgot Password?
+                        </button>
                         <button
-                            className="w-32 bg-blue-800 text-white px-4 py-2 rounded-lg cursor-pointer"
+                            className="w-full bg-blue-600 text-white px-4 py-2 rounded-lg cursor-pointer"
                             type ="submit"
-                            onClick={handleSubmit}>
-                                Submit
+                            >
+                                LogIn
+                        </button>
+                        <p className="text-gray-500 text-sm">
+                            Don't have an account?
+                            <button
+                            type='button'
+                                className="text-blue-600 ml-2 cursor-pointer"
+                                onClick={() => router.push('/auth/register')}>
+                                Register
+                            </button>
+                        </p>
+                        <hr className='border- overflow-visible w-full'/>
+                        <button 
+                            className="w-96 cursor-pointer flex items-center justify-center gap-2" 
+                            type="button">
+                                <FcGoogle size={20} />
+                                <span className='text-sm'>Login with Google</span>
                         </button>
                     </form>
-                        <button
-                            className='cursor-pointer'
-                            onClick={() => router.push('/auth/register')}>  
-                                Don't have an account? Register  
-                        </button>
                 </div>
             </div>                                                 
         </>
     )
 }
+
+export default LoginForm;
--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -2,52 +2,71 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import { FcGoogle } from 'react-icons/fc';
 
 export default function RegisterPage() {
     const router = useRouter();
 
     const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
         e.preventDefault();
+
         router.push('/auth/login');
     }
 
     return (
         <>
-            <div className='flex items-center justify-center min-h-screen'>
-                <div className="box-border border p-6 rounded-lg bg-blue-200">
+            <div className='flex items-center justify-center min-h-screen bg-blue-300'>
+                <div className="box-border  p-6 rounded-lg bg-white">
                     <div className="text-center text-lg font-bold">
-                        <h1>Register Page</h1>
+                        <h1>Sign Up</h1>
                     </div>
                     <form className="flex flex-col gap-4 mt-4 w-96 items-center">
                         <div>
                             <input
-                                className="w-96 px-4 py-2 rounded-lg focus:outline-none focus:ring-1 border bg-white" 
-                                type="name" 
-                                name="Name" 
-                                placeholder="Enter Your Name"
+                                className="w-96 px-4 py-2 rounded-lg border bg-white" 
+                                type="email" 
+                                name="Email" 
+                                placeholder="Email"
+                                required={true}
                             />
                         </div>
                         <div>
                             <input
-                                className="w-96 px-4 py-2 rounded-lg focus:outline-none focus:ring-1 border bg-white" 
-                                type="email" 
-                                name="Email" 
-                                placeholder="Enter Your Email"
+                                className="w-96 px-4 py-2 rounded-lg border bg-white" 
+                                type="password" 
+                                name="Password" 
+                                placeholder="Create Password"
                             />
                         </div>
                         <div>
                             <input 
-                                className="w-96 px-4 py-2 rounded-lg focus:outline-none focus:ring-1 border bg-white"
+                                className="w-96 px-4 py-2 rounded-lg border bg-white"
                                 type="password" 
                                 name="Password" 
-                                placeholder="Enter Your Password"
+                                placeholder="Confirm Password"
                             />
                         </div>
                         <button
-                            className="w-32 bg-blue-800 text-white px-4 py-2 rounded-lg cursor-pointer"
+                            className="w-full bg-blue-600 text-white px-4 py-2 rounded-lg cursor-pointer"
                             type ="submit"
                             onClick={handleSubmit}>
-                                Submit
+                                Sign Up
+                        </button>
+                        <p className="text-gray-500 text-sm">
+                            Already have an account?
+                            <button
+                                type='button'
+                                className="text-blue-600 ml-2 cursor-pointer"
+                                onClick={() => router.push('/auth/login')}>
+                                Login
+                            </button>
+                        </p>
+                        <hr className='border- overflow-visible w-full'/>
+                        <button 
+                            className="w-96 cursor-pointer flex items-center justify-center gap-2" 
+                            type="button">
+                                <FcGoogle size={20} />
+                                <span className='text-sm'>Login with Google</span>
                         </button>
                     </form>
                 </div>
